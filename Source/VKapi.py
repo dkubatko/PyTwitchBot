a48ef61c8a8b67f8103271eefa8cf904ed301d14
Daniil K <dkubatko@ucsd.edu>
2017-03-31 20:43:25 -0700

    minor updates

--- Source/VKapi.py
+++ Source/VKapi.py
@@ -1,6 +1,6 @@
 import vk
 def connectVK(scope = "wall"):
-	session = vk.AuthSession(app_id = '5813182', user_login = '89296301367',
-                             user_password = 'danjusha', scope = scope)
+	session = vk.AuthSession(app_id = 'XXXXXXXXXX', user_login = 'XXXXXXXXXXX',
+                             user_password = 'XXXXXXXXXXX', scope = scope)
 	api = vk.API(session)
 	return api
