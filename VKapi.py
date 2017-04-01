02be9516ceb52e4428a2b03797f0b005592b30b7
Daniil K <dkubatko@ucsd.edu>
2017-03-31 20:33:55 -0700

    deleted

--- VKapi.py
+++
@@ -1,6 +0,0 @@
-import vk
-def connectVK(scope = "wall"):
-	session = vk.AuthSession(app_id = '5813182', user_login = '89296301367',
-                             user_password = 'danjusha', scope = scope)
-	api = vk.API(session)
-	return api
